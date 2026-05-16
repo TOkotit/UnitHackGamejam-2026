0a824b3c1b26054a4e355859646c0ec6b6307a2a
TOkotit <105845895+TOkotit@users.noreply.github.com>
2026-05-16 12:28:31 +0500

    деш при атаке

--- Game/Assets/Script/Character/CharacterMovement.cs
+++ Game/Assets/Script/Character/CharacterMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -7,37 +8,54 @@ namespace Script.Character
     {
         [SerializeField] private GameObject SlashPrefab;
         [SerializeField] private float WalikingSpeed;
+        [SerializeField] private float DashSpeed = 15;
+        [SerializeField] private float jumpForce = 10f;
+        [SerializeField] private Rigidbody2D characterRB;
+        [SerializeField] private Transform groundCheck;
+        [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private float checkRadius = 0.2f;
         
-        private Rigidbody2D chara;
+        
+        private bool isAttacking = false;
+        private bool isGrounded;
+        public void Awake()
+        {
+            if(!characterRB)
+                characterRB = GetComponent<Rigidbody2D>();
+        }
 
-        
         public void Jump()
         {
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+            
+            if(isGrounded)
+                characterRB.linearVelocity = new Vector2(0, jumpForce);
         }
 
         public void Move(Vector2 moveDirection)
         {
-            transform.Translate(moveDirection * (WalikingSpeed * Time.fixedDeltaTime));
+            if(!isAttacking)
+                transform.Translate(moveDirection * (WalikingSpeed * Time.fixedDeltaTime));
             
         }
 
         public void Attack(Vector2 mouseScreenPosition)
         {
-
+            isAttacking = true;
             if (Camera.main == null) return;
             var mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
 
             var attackDirectionX = mouseWorldPosition.x > transform.position.x ? 1f : -1f;
-
+            DashWithAttack(attackDirectionX);
             var spawnPosition = transform.position + new Vector3(attackDirectionX * 2f, 0f, 0f);
             var splash = Instantiate(SlashPrefab, spawnPosition, Quaternion.identity);
+            splash.transform.SetParent(transform);
             var currentScale = splash.transform.localScale;
             if (attackDirectionX < 0)
                 currentScale.x = -Mathf.Abs(currentScale.x);
             else
                 currentScale.x = Mathf.Abs(currentScale.x);
             splash.transform.localScale = currentScale;
-            DashWithAttack(attackDirectionX);
             StartCoroutine(AttackAnimtation(splash));
         }
         
@@ -45,17 +63,18 @@ namespace Script.Character
         {
             yield return new WaitForSeconds(0.2f);
             Destroy(splash);
+            isAttacking = false;
         }
-        
-        public void DashWithAttack(float attackDirection)
+
+        private void DashWithAttack(float attackDirection)
         {
             if (attackDirection > 0)
             {
-                transform.position += new Vector3(WalikingSpeed * Time.fixedDeltaTime * 5, 0f, 0f);
+                transform.position += new Vector3(DashSpeed, 0f, 0f);
             }
             else
             {
-                transform.position += new Vector3(-WalikingSpeed * Time.fixedDeltaTime * 5, 0f, 0f);
+                transform.position += new Vector3(-DashSpeed, 0f, 0f);
 
             }
             
@@ -63,7 +82,7 @@ namespace Script.Character
         
         private void FixedUpdate()
         {
-            
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
         }
         
     }
